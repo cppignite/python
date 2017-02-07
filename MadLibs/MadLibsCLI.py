3f69fe0d7ef8a2b751744bf2702c6bc13dd18d00
Dave Luk <dluk960510@gmail.com>
2017-02-06 21:39:59 -0800

    Add files via upload

---
+++ MadLibs/MadLibsCLI.py
@@ -0,0 +1,22 @@
+vocabList = [] #initialize a empty list
+
+#naive approach 1
+vocabList[0] = input("Please put in an adj: ")
+vocabList[1] = input("Please put in an adj: ")
+print("You are in a " + vocabList[0] + " forest, and you are " + vocabList[1] + ".")
+#problems: Rigid length, cumbersome to type it all out
+
+#naive approach 2
+for x in range(1, desiredNumber):
+    vocabList[x] = input("Please put an adj: ");
+print("You are in a " + vocabList[0] + " forest, and you are " + vocabList[1] + ".")
+#problem: still typing it out
+
+#proper approach
+for x in range(1, desiredNumber):
+    vocabList[x] = input("Please put an adj: ");
+story = "You are in a {} forest, and you are {}."
+print(story.format(*vocabList))
+
+input("Press Enter to Quit") 
+
