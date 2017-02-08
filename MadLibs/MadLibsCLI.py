54696280ada8badf0525ab83b839b9c97e176520
Dave Luk <dluk960510@gmail.com>
2017-02-07 20:38:03 -0800

    Corrected loops indexing, initialize list properly

--- MadLibs/MadLibsCLI.py
+++ MadLibs/MadLibsCLI.py
@@ -1,4 +1,4 @@
-vocabList = [] #initialize a empty list
+vocabList = ["",""] #initialize a empty list of size 2
 
 #naive approach 1
 vocabList[0] = input("Please put in an adj: ")
@@ -7,13 +7,13 @@ print("You are in a " + vocabList[0] + " forest, and you are " + vocabList[1] + 
 #problems: Rigid length, cumbersome to type it all out
 
 #naive approach 2
-for x in range(1, desiredNumber):
+for x in range(0, desiredNumber):
     vocabList[x] = input("Please put an adj: ");
 print("You are in a " + vocabList[0] + " forest, and you are " + vocabList[1] + ".")
 #problem: still typing it out
 
 #proper approach
-for x in range(1, desiredNumber):
+for x in range(0, desiredNumber):
     vocabList[x] = input("Please put an adj: ");
 story = "You are in a {} forest, and you are {}."
 print(story.format(*vocabList))
