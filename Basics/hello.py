559aed33e06ed500f53e3c7edcc7de7b87afd3dc
agraca <anthonygraca@gmail.com>
2017-12-19 12:26:49 -0800

    added example code for week 1

---
+++ Basics/hello.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for my name.
+
+print('Hello World!')
+print('What is your name?')   # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
