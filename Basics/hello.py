5f84394ec8181d4008cb68473e614ee96011c5b9
agraca <anthonygraca@gmail.com>
2017-12-19 12:38:46 -0800

    added age to prog

--- Basics/hello.py
+++ Basics/hello.py
@@ -6,3 +6,7 @@ myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
+
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
