6f9787fefbce53c417e692c72394bb4dcbbdd164
agraca <anthonygraca@gmail.com>
2017-12-19 18:10:09 -0800

    added mini-activity for if-else

---
+++ Control_Flow/welcome.py
@@ -0,0 +1,10 @@
+# mini activity for if-else
+
+print('What is your name?')
+name = input()
+
+print('\nPlease enter your name')
+if name == input():
+  print('Hello, ' + name)
+else:
+  print('Hello, stranger')
