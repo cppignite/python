f3b18bb9397984a3c34065450a4e05a50c0a4262
agraca <anthonygraca@gmail.com>
2017-12-19 18:48:58 -0800

    added mini-activity with a while loop

---
+++ Control_Flow/can_you_feel_it_mr_krabs.py
@@ -0,0 +1,14 @@
+# mini-activity for a while loop
+
+print('This is with an if statement')
+count = 0
+if count < 5:
+  print('Can You Feel It, Mr. Krabs?')
+  count = count + 1
+
+
+print('\nThis is with a while statement')
+count = 0
+while count < 5:
+  print('Money')
+  count = count + 1
