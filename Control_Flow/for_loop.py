e05d2e95f095b50b1d8426dd491f7182c54c01cd
agraca <anthonygraca@gmail.com>
2017-12-20 21:33:06 -0800

    for loop and while loop mini-activity

---
+++ Control_Flow/for_loop.py
@@ -0,0 +1,12 @@
+# For loop mini-activity, Sum from 1 to 100
+sum1 = 0
+for i in range(101):
+  sum1 = sum1 + 1
+print("The sum from 1 to 100 is: " + str(sum1)
+
+sum2 = 0
+i = 0
+while i < 101:
+  sum2 = sum2 + i
+  i++
+print("The sum from 1 to 100 is: " + str(sum2)
