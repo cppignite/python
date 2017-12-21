392cb107802f40df2f5b0124bd7f3c84f66b1b78
agraca <anthonygraca@gmail.com>
2017-12-20 21:46:35 -0800

    fixed syntax error

--- Control_Flow/for_loop.py
+++ Control_Flow/for_loop.py
@@ -2,11 +2,11 @@
 sum1 = 0
 for i in range(101):
   sum1 = sum1 + 1
-print("The sum from 1 to 100 is: " + str(sum1)
+print("The sum from 1 to 100 is: " + str(sum1))
 
 sum2 = 0
 i = 0
 while i < 101:
   sum2 = sum2 + i
   i += 1
-print("The sum from 1 to 100 is: " + str(sum2)
+print("The sum from 1 to 100 is: " + str(sum2))
