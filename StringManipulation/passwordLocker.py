69239b30f1b8216d5ee226bb3b90d8f65d686ba3
anthonygraca <anthonygraca@gmail.com>
2017-12-22 18:47:54 -0800

    added first two steps of instruction

---
+++ StringManipulation/passwordLocker.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+"""
+passwordLocker.py - An insecure password locker program.
+1.) Choose data structure - We will store name and password into a dictionary
+                            as a key-value pair
+2.) Handle Command Line Arguments - sys.argv is a list where the first item is
+                                    the name of the program and the second item
+                                    is the first command line argument
+
+"""
+PASSWORDS = {'email': 'test',
+             'blog': 'test1'
+             'luggage': 'test2'}
+
+
