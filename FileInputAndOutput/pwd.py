670a8ed6a7875e0119dee3f3e574f8f4159f5603
anthonygraca <anthonygraca@gmail.com>
2017-12-23 14:52:43 -0800

    added activity on working directory and mini-activity for students

---
+++ FileInputAndOutput/pwd.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# tells me what's my working directory
+
+import os
+print(os.getcwd())
+
+# Let's make a script to make us lazy. I don't want to keep on moving directory
+# to get to my workspace
+# use os.chdir('some dir')
