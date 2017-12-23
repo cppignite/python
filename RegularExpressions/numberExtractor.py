75079a670562e821ff4ab074a1d86d41f32185d3
anthonygraca <anthonygraca@gmail.com>
2017-12-23 14:22:54 -0800

    completed email and phone number extractor

--- RegularExpressions/numberExtractor.py
+++ RegularExpressions/numberExtractor.py
@@ -1,4 +1,5 @@
-'''
+#! /usr/bin/python3
+"""
 This project will take the phone numbers and email addresses from a webpage or
 document and store it in a string.
 
@@ -16,4 +17,47 @@ Remember: Break apart a big project into smaller problems and do them all
           seperately. That makes the project managable in terms you already
           know
 
-'''
+We will use this script on some poor smuck's contact page
+
+"""
+# Step 0.) import pyperclip and regular expression modules
+import pyperclip, re
+
+# Step 1.) Create REGEX for phone numbers
+phoneRegex = re.compile(r'''(
+             (\d{3}|\(\d{3}\))?             # area code
+             (\s|-|\.)?                     # seperator 
+             (\d{3})                        # first 3 digits
+             (\s|-|\.)                      # seperator
+             (\d{4})                        # last 4 digits
+             (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+             )''', re.VERBOSE)
+
+# Step 2.) Create REGEX for email addresses
+emailRegex = re.compile(r'''(
+             [a-zA-Z0-9._%+-]+              # username
+             @                              # @ symbol
+             [a-zA-Z0-9.-]+                 # domain name
+             (\.[a-zA-Z]{2-4})              # dot-something
+             )''', re.VERBOSE)
+
+# Step 3.) Find all matches in clipboard text
+text = str(pyperclip.paste())
+print(text)
+matches = []
+for groups in phoneRegex.findall(text):
+  phoneNum = '-'.join([groups[1], groups[3], groups[5]]) # String building
+  if groups[8] != '':
+    phoneNum += ' x' + groups[8]
+  matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+  matches.append(groups[0])
+
+# Step 4.) Join the Matches into a String for the Clipboard
+if len(matches) > 0:
+  pyperclip.copy('\n'.join(matches))
+  print('Copied to clipboard:')
+  print('\n'.join(matches))
+else:
+  print('No phone numbers or email addresses found.')
+
