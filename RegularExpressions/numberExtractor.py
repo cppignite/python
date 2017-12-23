ae70a518ad79622704d762c8b07766ecdcb78959
anthonygraca <anthonygraca@gmail.com>
2017-12-23 14:02:08 -0800

    added project description

---
+++ RegularExpressions/numberExtractor.py
@@ -0,0 +1,19 @@
+'''
+This project will take the phone numbers and email addresses from a webpage or
+document and store it in a string.
+
+Tasks of the program:
+  * Get text off the clipboard
+    - We can do this with pyperclip
+  * Find all phone numbers and email addresses in text
+    - We can create two regexes, one for phone numbers and one for emails
+    - We need to find all matches, not just one match
+  * Paste them onto the clipboard
+    - We need to format the strings into a single string
+    - Display some message if no matches were found
+
+Remember: Break apart a big project into smaller problems and do them all
+          seperately. That makes the project managable in terms you already
+          know
+
+'''
