74582283b4304bb5dd1d2e76471ebf970e323a4a
anthonygraca <anthonygraca@gmail.com>
2017-12-22 18:52:29 -0800

    added instructions to copy password

--- StringManipulation/passwordLocker.py
+++ StringManipulation/passwordLocker.py
@@ -6,6 +6,11 @@ passwordLocker.py - An insecure password locker program.
 2.) Handle Command Line Arguments - sys.argv is a list where the first item is
                                     the name of the program and the second item
                                     is the first command line argument
+3.) Copy the right password 
+    a.) See if the account exists in the PASSWORDS dictionary as a key 
+       i.) If so, copy the key's value to the clipboard. (remember to import
+           pyperclip so you can use pyperclip.copy()). We don't need the
+           account variable because we could just use sys.argv[1]
 
 """
 PASSWORDS = {'email': 'test',
