4e135a4b8065b8924933f5622ef8e7f6b7d98cb4
anthonygraca <anthonygraca@gmail.com>
2017-12-22 21:58:28 -0800

    -S
    added logic

--- StringManipulation/passwordLocker.py
+++ StringManipulation/passwordLocker.py
@@ -24,3 +24,9 @@ if len(sys.argv) < 2:
   sys.exit()
 
 account = sys.argv[1]  # first command line arg is the account name
+
+if account in PASSWORDS:
+  pyperclip.copy(PASSWORDS[account])
+  print('Password for ' + account + ' copied to clipboard.')
+else:
+  print('There is no account named ' + account
