aa17e72bbcd217875884b8541731eb52e40dfe82
anthonygraca <anthonygraca@gmail.com>
2017-12-22 18:54:03 -0800

    added warning for pip

--- StringManipulation/passwordLocker.py
+++ StringManipulation/passwordLocker.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 """
 passwordLocker.py - An insecure password locker program.
+*Make sure you use pip to install pyperclip beforehand
 1.) Choose data structure - We will store name and password into a dictionary
                             as a key-value pair
 2.) Handle Command Line Arguments - sys.argv is a list where the first item is
@@ -17,7 +18,7 @@ PASSWORDS = {'email': 'test',
              'blog': 'test1'
              'luggage': 'test2'}
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
   print('Usage: python passwordLocker.py [account] - copy account password')
   sys.exit()
