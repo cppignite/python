697ae2f6218ad4143ce9a15246f14ec55dd6afb7
anthonygraca <anthonygraca@gmail.com>
2017-12-24 22:44:19 -0800

    finished program

--- FileInputAndOutput/randomQuizGenerator.py
+++ FileInputAndOutput/randomQuizGenerator.py
@@ -25,13 +25,14 @@ Here is what the program does:
 Extensions:
   * Create a Text User Interface allowing how many questions to use and how
     many tests to generate
+  * Ask what to name exam
   * Make it so the teacher can input any question and answer on a text document
     and the program will read from that document to generate the dictionary.
     ** Adapt Text User Interface to ask for file input of the questions and ans
   * Create a Graphical User Interface
 """
 # Step 0: Import modules
-import random, re
+import random, re, os
 
 # Step 1: Store the Quiz Data in a Dictionary
 def readQuizData():
@@ -52,3 +53,54 @@ def readQuizData():
   return matches 
 
 capitals = readQuizData()
+
+# Step 2: Create the Quiz File and Shuffle The Question Order
+# Generate x=35 quiz files
+os.makedirs('exams')    # makes 'exam' folder
+os.makedirs('answers')  # makes 'answers' folder
+numOfExams = 35
+for quizNum in range(numOfExams):
+  # Create the quiz and answer key files
+  quizFile = open('exams/capitalsquiz%s.txt' % (quizNum + 1), 'w')
+  answerKeyFile = open('answers/capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+  # Write out the header for the quiz
+  quizFile.write('Name:\n\nDate:\n\n')
+  quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+  quizFile.write('\n\n')
+
+  # Shuffle the order of the states.
+  states = list(capitals.keys())
+  random.shuffle(states)
+
+# Step 3: Create the Answer Options
+# Loop through all 50 states, making a question for each
+  for questionNum in range(50):
+
+    # Get right and wrong answers.
+    # Get correct answer from our capitals dictionary
+    correctAnswer = capitals[states[questionNum]]
+    # Duplicate all of the answers of the capitals dictionary
+    wrongAnswers = list(capitals.values())
+    # Delete the correct answer from the wrongAnswers list
+    del wrongAnswers[wrongAnswers.index(correctAnswer)]
+    # Select three random wrong answers
+    wrongAnswers = random.sample(wrongAnswers, 3)
+    # Add in correct answer for our 4 possible answers to a question
+    answerOptions = wrongAnswers + [correctAnswer]
+    # Randomize the answer options to the correct answer isn't D
+    random.shuffle(answerOptions)
+
+# Step 4: Write content to Quiz and Answer Key Files
+    # Write the question and the answer options to the quiz file
+    quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, 
+                   states[questionNum]))
+    for i in range(4):
+      quizFile.write('    %s. %s \n' % ('ABCD'[i], answerOptions[i]))
+    quizFile.write('\n')
+
+    # Write the answer key to a file
+    answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[
+                        answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
