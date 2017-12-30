b3e99045ffdc3194e87bd033c6a854e7218a608d
anthonygraca <anthonygraca@gmail.com>
2017-12-29 23:50:55 -0800

    defined project description

---
+++ Libraries/OrganizingFiles/pbackup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+'''
+This project will create a ZIP file of an entire folder. This is a form
+of version control where you can take snapshots of your folder so you can
+keep different versions. The ZIP files will also increment each time it is 
+called. (e.g backup_1.zip, backup_2.zip, etc.)
+
+Future Extensions: Also put date on folder as well
+'''
+
