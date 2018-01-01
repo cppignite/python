304b0b3f32e32fdbc088010992667544f72f188e
agraca <anthonygraca@gmail.com>
2017-12-31 22:38:23 -0800

    added if statement

--- Modules/WebScraping/requestExample.py
+++ Modules/WebScraping/requestExample.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 """
   The requests module allows you to download files from the Web.
 
@@ -6,5 +7,16 @@
                  object which contains the response the web server gave to
                  your request
 
-  request.status_code and requests.codes.ok check if the request was successful
+  res.status_code and requests.codes.ok check if the request was successful
+  len(res.text) shows how many characters
+  res.text is list of words of the text
+  res.raise_for_status() - raises an exception if fail
 """
+
+import pyperclip
+
+weburl = pyperclip.paste()
+res = requests.get(weburl)
+
+if res.status_code == requests.codes.ok:
+  print('Request Successful')
