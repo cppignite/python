af8f1496b0f3b35367121310bfb8be1a4b5175ff
agraca <anthonygraca@gmail.com>
2017-12-31 22:28:22 -0800

    added notice

--- Modules/WebScraping/mapit.py
+++ Modules/WebScraping/mapit.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 """
+ ** Make sure you install pyperclip with pip
+
  Script opens a google maps webpage with an address either on command line or
  on clipboard
  Demonstrates power of  webbrowser.open(url), which opens a web browser
