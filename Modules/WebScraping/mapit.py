8bd36c317fd736a6257161905ec6acce4e18d14b
agraca <anthonygraca@gmail.com>
2017-12-31 22:23:54 -0800

    implemented cmd line arg

--- Modules/WebScraping/mapit.py
+++ Modules/WebScraping/mapit.py
@@ -1,13 +1,23 @@
 #! /usr/bin/python3
-# Script opens a google maps webpage with an address either on command line or
-# on clipboard
-# Demonstrates power of  webbrowser.open(url), which opens a web browser
-#
-# Extra Projects: Open browser to what you usually open to
+"""
+ Script opens a google maps webpage with an address either on command line or
+ on clipboard
+ Demonstrates power of  webbrowser.open(url), which opens a web browser
 
-import webbrowser
+ Usage: mapit <Address>
+ Example: mapit 3801 W Temple Ave, Pomona, CA 91768
+
+ Extra Projects: Open browser to what you usually open to
+"""
+
+import webbrowser, sys, pyperclip
 
 # Step 1: Figure Out the URL
+# The idea is that you can search by putting the address on the url of GMaps
+if len(sys.argv) > 1:
+  # Get Address from command line, we want to have the addr as a single string
+  # We don't want the program name so we start from argv[1:]
+  address = ' '.join(sys.argv[1:])
 
 # Step 2: Handle the Command Line Arguments
 
