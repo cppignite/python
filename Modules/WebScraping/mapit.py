48fc9e5bf02d88b41d56a99ce78fb30479d7b8cf
agraca <anthonygraca@gmail.com>
2017-12-31 22:16:35 -0800

    added mapit.py description

---
+++ Modules/WebScraping/mapit.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+# Script opens a google maps webpage with an address either on command line or
+# on clipboard
+# Demonstrates power of  webbrowser.open(url), which opens a web browser
+
+import webbrowser
