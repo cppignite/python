fece395a78e0c7ba8c9aa15fa5d23e1e6f7f90ae
agraca <anthonygraca@gmail.com>
2017-12-31 22:18:43 -0800

    added steps

--- Modules/WebScraping/mapit.py
+++ Modules/WebScraping/mapit.py
@@ -2,5 +2,13 @@
 # Script opens a google maps webpage with an address either on command line or
 # on clipboard
 # Demonstrates power of  webbrowser.open(url), which opens a web browser
+#
+# Extra Projects: Open browser to what you usually open to
 
 import webbrowser
+
+# Step 1: Figure Out the URL
+
+# Step 2: Handle the Command Line Arguments
+
+# Step 3: Handle the Clipboard Content and Launch Browser
