97b0c7bc01dcdfcd394e4795d0efc664d925399c
agraca <anthonygraca@gmail.com>
2017-12-31 22:33:21 -0800

    added description
    Signed-off-by: agraca <anthonygraca@gmail.com>

---
+++ Modules/WebScraping/requestExample.py
@@ -0,0 +1,10 @@
+"""
+  The requests module allows you to download files from the Web.
+
+  functions used:
+  requests.get() takes a string of a URL to download. - Returns a Response
+                 object which contains the response the web server gave to
+                 your request
+
+  request.status_code and requests.codes.ok check if the request was successful
+"""
