3c46d069cecc080a9ed852998615cbecc8eceea9
anthonygraca <anthonygraca@gmail.com>
2018-01-05 15:17:26 -0800

    added description for google search

---
+++ Modules/BeautifulSoup/google.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+'''
+  Whenever people do Google searches, they search and open several new tabs
+  by middle-clicking on the first few top links. That's a lot of work. I want
+  to just type the search term on my command line and all the top links 
+  automatically open on my computer. Let's do that
+'''
